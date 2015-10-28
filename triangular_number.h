6abc8e8a1b7397828778bea1ca379de1e2a6818c
Makram Kamaleddine <makram.kd@gmail.com>
2015-10-28 13:50:13 -0400

    Adding an implementation of a compile time triangular number.

--- triangular_number.h
+++ triangular_number.h
@@ -5,4 +5,21 @@
 #ifndef MY_MPL_TRIANGULAR_NUMBER_H
 #define MY_MPL_TRIANGULAR_NUMBER_H
 
+namespace mpl {
+
+    template<int N>
+    struct triangular_number {
+        static constexpr auto value = triangular_number<N - 1>::value + N;
+    };
+
+    template<>
+    struct triangular_number<1> {
+        static constexpr auto value = 1;
+    };
+
+    template<>
+    struct triangular_number<0> {
+        static constexpr auto value = 0;
+    };
+}
 #endif //MY_MPL_TRIANGULAR_NUMBER_H

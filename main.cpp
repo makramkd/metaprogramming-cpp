69416012d62bff4e3c374ee83d514f3c54550146
Makram Kamaleddine <makram.kd@gmail.com>
2015-10-28 13:51:26 -0400

    Updating main with some compile time tests.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,11 @@
 #include <iostream>
 
-#include "power.h"
-#include "factorial.h"
-#include "abs.h"
-#include "intops.h"
+#include "if.h"
 
 int main() {
 
-    const int N = 18;
-    const int M = 42;
-    auto val1 = mpl::add<N, M>::value;
-    auto val2 = mpl::sub<N, M>::value;
-    auto val3 = mpl::div<N, M>::value;
-    auto val4 = mpl::mod<N, M>::value;
-    auto val5 = mpl::mult<N, M>::value;
+    constexpr auto val = 3;
+    mpl::if_t<(val < 2), decltype(val), unsigned> val2 = 3;
 
-    std::cout << val1 << " " << val2 << " " << val3 << " ";
-    std::cout << val4 << " " << val5 << std::endl;
+    
 }

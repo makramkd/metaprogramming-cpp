cd1ce18691c3f82385aa835df9c2d0e9e6fbaaf4
Makram Kamaleddine <makram.kd@gmail.com>
2015-10-28 13:51:02 -0400

    Basic inspection mechanisms like array ranks and the identity metafunction.

---
+++ inspect.h
@@ -0,0 +1,38 @@
+//
+// Created by Makram Kamaleddine on 10/28/15.
+//
+
+#ifndef MY_MPL_INSPECT_H
+#define MY_MPL_INSPECT_H
+
+#include <stddef.h>
+
+namespace mpl {
+
+    // a metafunction that gets the compile time
+    // rank of an array
+
+    // base case: T is not an array type (i.e its a scalar type)
+    template<typename T>
+    struct rank {
+        enum { value = 0u };
+    };
+
+    // recursion: U could be an array type as well
+    template<class U, size_t N>
+    struct rank<U[N]> {
+        enum { value = 1u + rank<U>::value};
+    };
+
+    // identity metafunction
+    template<typename T>
+    struct type_id {
+        using type = T;
+    };
+
+    // alias template
+    // for better usability
+    template<typename T>
+    using type_of = typename type_id<T>::type;
+}
+#endif //MY_MPL_INSPECT_H

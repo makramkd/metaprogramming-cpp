ec3b9d9f814cf0f83f8331c91754a93448be21e2
Makram Kamaleddine <makram.kd@gmail.com>
2015-10-28 16:58:49 -0400

    Cleaner implementations of is_* traits.

---
+++ traits.h
@@ -0,0 +1,35 @@
+//
+// Created by Makram Kamaleddine on 10/28/15.
+//
+
+#ifndef MY_MPL_TRAITS_H
+#define MY_MPL_TRAITS_H
+
+#include "inspect.h"
+
+namespace mpl {
+
+    // we will use is_any_of to make life
+    // easier
+    template<typename T>
+    using is_floating = is_any_of<T, float, const float,
+            volatile float, const volatile float, double, const double,
+            volatile double, const volatile double>;
+
+    template<typename T>
+    using is_integral = is_any_of<T, bool, char, char16_t, char32_t,
+            short, int, long, long long, unsigned char, unsigned short,
+            unsigned int, unsigned long long,
+            const char, volatile char, const volatile char,
+            const char16_t, volatile char16_t, const volatile char16_t,
+            const char32_t, volatile char32_t, const volatile char32_t,
+            const short, volatile short, const volatile short>; // many more
+
+    // re-implementing is_void
+    template<typename T>
+    using is_void = is_any_of<T, void, volatile void, const void,
+            const volatile void>;
+    
+}
+
+#endif //MY_MPL_TRAITS_H
